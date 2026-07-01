4920fa4000c365cc706977eebe5bdbb6cdd2c83f
danialcala94 <danielalcalavalera@gmail.com>
2026-07-02 01:54:12 +0700

    Project refactored due to migration to 'ai-simple-engine-runtime-transformers-musicgen'. Removed tests. Poetry lock file updated. New version.

--- src/ai_simple_engine_musicgen/models/loaded_musicgen_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoProcessor, MusicgenForConditionalGeneration
-from dataclasses import dataclass
-
-
-@dataclass(frozen = True)
-class LoadedMusicgenModel:
-
-    processor: AutoProcessor
-    network: MusicgenForConditionalGeneration
